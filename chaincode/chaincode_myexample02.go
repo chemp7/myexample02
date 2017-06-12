d149eeb57cdc44ad5adc7243f94270fb032ebe2f
Takeshi Yoshida <chemp7@gmail.com>
2017-06-12 18:58:49 +0900

    Create chaincode_myexample02.go

--- chaincode/chaincode_myexample02.go
+++ chaincode/chaincode_myexample02.go
@@ -56,7 +56,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
 
-	err = stub.PutState("ID", A + ", " + B)
+	var id = A + ", " + B;
+	err = stub.PutState("ID",  []byte(id))
 	if err != nil {
 		return nil, errors.New("ID SET ERROR!")
 	}
