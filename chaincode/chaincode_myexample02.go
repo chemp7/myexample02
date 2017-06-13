52c935d2496a2484aa02cd179316cc810f1a1be9
Takeshi Yoshida <chemp7@gmail.com>
2017-06-13 12:05:23 +0900

    Create chaincode_myexample02.go

--- chaincode/chaincode_myexample02.go
+++ chaincode/chaincode_myexample02.go
@@ -251,7 +251,13 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		fmt.Printf("Query Response:%s\n", jsonResp)
 		return param, nil
 	}
-	retirm []byte("A"), nil
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	retirm valAsbytes, nil
 }
 
 func main() {
